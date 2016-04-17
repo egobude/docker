81e8b57c8ad3c1de881f1278afca307b41bf5eb0
Benedikt Schmitz <zeroboh@gmail.com>
2016-04-17 20:16:25 +0200

    [TASK] Füge Shopware 5.1.5 hinzu

---
+++ shopware/container-files/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -e /var/www/shopware/shopware.php ]; then
+    echo "Shopware not found, installing..."
+    rm -f /var/www/shopware/index.html \
+    && unzip -d /var/www/shopware /tmp/shopware.zip \
+    && chown -R www-data /var/www/shopware/var/log \
+                         /var/www/shopware/var/cache \
+                         /var/www/shopware/web \
+                         /var/www/shopware/recovery \
+                         /var/www/shopware/themes/Frontend \
+                         /var/www/shopware/config.php \
+                         /var/www/shopware/cache \
+                         /var/www/shopware/files/* \
+                         /var/www/shopware/engine/Shopware/Plugins/Community/ \
+                         /var/www/shopware/engine/Shopware/Plugins/Default/ \
+                         /var/www/shopware/media/*
+    echo "done"
+fi
+
+exec /usr/sbin/apache2ctl -D FOREGROUND
